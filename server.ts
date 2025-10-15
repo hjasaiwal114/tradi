23c2994fa5c1de15e1506ca83a7152aacc5dd6a6
himanshu <hjasaiwal114@gmail.com>
2025-10-15 08:46:39 +0530

    add user routes

--- server.ts
+++ server.ts
@@ -1,19 +1,25 @@
 import dotenv from 'dotenv';
-dotenv.config()
+dotenv.config();
 import express from 'express';
 import cookieParser from 'cookie-parser';
-import {config} from './src/config';
+import { config } from './src/config';
 import authRoutes from './src/api/routes/authRoutes';
+import userRoutes from './src/api/routes/userRoutes';
 
 const app = express();
 
+// middleware;;;
 app.use(express.json());
 
 app.use(cookieParser(config.cookieSecret));
 
+// ----Api routes----
 app.use('/api/v1', authRoutes);
+app.use('api/v1', userRoutes);
+
+
 
 app.listen(config.port, () => {
-    console.log(`Server is running on https://localhost:${config.port}`);
-    console.log(`Current enviroment: ${config.nodeEnv}`);
+  console.log(`Server is running on https://localhost:${config.port}`);
+  console.log(`Current enviroment: ${config.nodeEnv}`);
 });
