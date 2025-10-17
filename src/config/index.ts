e4679e1eff634174a40ffd292d26c9bb1e657692
himanshu <hjasaiwal114@gmail.com>
2025-10-17 06:51:03 +0530

    polling server

--- src/config/index.ts
+++ src/config/index.ts
@@ -10,4 +10,6 @@ export const config = {
     jwtSecret: process.env.JWT_SECRET || 'supersecret',
     cookieSecret: process.env.COOKIE_SECRET || 'cookiesecret',
     resendApiKey: process.env.RESEND_API_KEY,
+    redisHost: process.env.REDIS_HOST || '127.0.0.1',
+    redisPort: parseInt(process.env.REDIS_PORT || '6379', 10), 
 }
